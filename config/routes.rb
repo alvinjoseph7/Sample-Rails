28299f67447beb9fd629c5775c98e973fcb60cfe
alvinjoseph7 <alvin9joseph@gmail.com>
2021-04-30 11:22:56 +0530

    some final changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,10 @@
 Rails.application.routes.draw do
-  get 'users/new'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # get 'static_pages/home'
   root 'static_pages#home'
-  # get 'static_pages/help'
-  get '/help', to: 'static_pages#help'
-  # get '/help', to: 'static_pages#help', as:'helf'
+  get '/help', to: 'static_pages#help'# , as:'helf' -> helf_path 
   get '/about', to: 'static_pages#about'
   get '/contact', to: 'static_pages#contact'
-  get '/signup', to: 'users#new'
+  get '/signup', to: 'users#new'    #  get 'users/new' -> get users_new_url
 end
   
