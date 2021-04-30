c97e7e965c2313a585e6937251855bf15b208c80
alvinjoseph7 <alvin9joseph@gmail.com>
2021-04-30 11:01:35 +0530

    add css layouts and user controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
-  root 'static_pages#home'
-  get 'static_pages/home'
-  get 'static_pages/help'
-  get 'static_pages/about'
-  get 'static_pages/contact'
+  get 'users/new'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # get 'static_pages/home'
+  root 'static_pages#home'
+  # get 'static_pages/help'
+  get '/help', to: 'static_pages#help'
+  # get '/help', to: 'static_pages#help', as:'helf'
+  get '/about', to: 'static_pages#about'
+  get '/contact', to: 'static_pages#contact'
+  get '/signup', to: 'users#new'
 end
+  
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -2,7 +2,7 @@ require "test_helper"
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   def setup
-    @common_title = "| Ruby on Rails Tutorial Sample App"
+    @common_title = "Ruby on Rails Tutorial Sample App"
   end
 
   test "should get root" do  
@@ -11,26 +11,26 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should get home" do
-    get static_pages_home_url
+    get root_path
     assert_response :success
-    assert_select "title", "Home #{@common_title}"
+    assert_select "title", @common_title
   end
 
   test "should get help" do
-    get static_pages_help_url
+    get help_path
     assert_response :success
-    assert_select "title", "Help #{@common_title}"
+    assert_select "title", "Help | #{@common_title}"
   end
   
   test "should get about" do
-    get static_pages_about_url
+    get about_path
     assert_response :success
-    assert_select "title", "About #{@common_title}"
+    assert_select "title", "About | #{@common_title}"
   end
 
   test "should get contact" do
-    get static_pages_contact_url
+    get contact_path
     assert_response :success
-    assert_select "title", "Contact #{@common_title}"
+    assert_select "title", "Contact | #{@common_title}"
   end
 end
